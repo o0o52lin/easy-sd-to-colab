ef91ebe4ceaa6e7ea13561bacf5745f500dc2a77
o0o52lin <345399240@qq.com>
2023-06-16 22:17:17 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,6 +3,8 @@ if ! command -v jq &> /dev/null
 then
   echo "jq not found, installing..."
   apt -y install jq
+else
+  echo "jq ok"
 fi
 JSON_WEBUI="{\"webui\":{\"url\":\"http\"}}"
-echo $($JSON_WEBUI | jq -r '.webui.url')
+echo $("$JSON_WEBUI" | jq -r '.webui.url')
