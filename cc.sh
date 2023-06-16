2daa43ac148de8e69f3d24d48004adc428796206
o0o52lin <345399240@qq.com>
2023-06-16 23:16:19 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-json='{
-   "checkpoints": [
-      {
-         "filename":"majicmixRealistic_v6.safetensors",
-         "url": "https://civitai.com/api/download/models/94640"
-      }
-   ]
-}'
+checkpoints='[
+   {
+      "filename":"majicmixRealistic_v6.2",
+      "url": "https://civitai.com/api/download/models/1"
+   },
+   {
+      "filename":"majicmixRealistic_v6.safetensors",
+      "url": "https://civitai.com/api/download/models/2"
+   }
+]'
 
-echo $json | jq -r '.checkpoints[] | "\(.filename) \(.url)"'
+# 使用 jq 解析 JSON 数据
+for checkpoint in $(echo "${checkpoints}" | jq -r '.[] | @base64'); do
+    # 解码 base64 编码的 JSON 数据
+    _jq() {
+        echo "${checkpoint}" | base64 --decode | jq -r "${1}"
+    }
+
+    filename=$(_jq '.filename')
+    url=$(_jq '.url')
+
+    echo "filename: ${filename}, url: ${url}"
+done
