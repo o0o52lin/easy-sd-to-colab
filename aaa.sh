685dd16b8da10ecb396e90ac423f4b428e937f5f
o0o52lin <345399240@qq.com>
2023-06-16 22:47:07 +0800

    Update and rename a.sh to aaa.sh

--- aaa.sh
+++ aaa.sh
@@ -2,9 +2,9 @@
 #判断是否安装 json解析工具“jq”
 echo  `command -v jq`
 if [ `command -v jq` ];then
-    echo 'jq 已经安装了0'
+    echo 'jq 已经安装了00'
     JSON_WEBUI='{"webui":{"url":"http"}}'
-    echo $("$JSON_WEBUI" | jq -r '.webui.url')
+    echo "$JSON_WEBUI" | jq -r '.webui.url'
 else
     echo 'jq 未安装,开始安装json解析工具'
     #安装jq
