4f33299605e4a39aa38f4d444374d8b9292b5047
o0o52lin <345399240@qq.com>
2023-06-25 14:30:13 +0800

    Update and rename test0.sh to test.sh

--- test.sh
+++ test.sh
@@ -5,8 +5,10 @@ if [[ ! -z $location ]]; then
   url=$location
 fi
 echo "url: $url"
-a='curl -Is -X "GET" <url>'
-a=${a/<url>/"$url"}
+a='curl -Is -X "GET" "<url>"'
+b="import os; bash_script='curl -Is -X \"GET\" \"<url>\"'; var_name='<url>'; var_value='"$url"'; bash_script=bash_script.replace(var_name,var_value); print(bash_script)"
+echo $b
+a=$(python -c "import os; bash_script='curl -Is -X \"GET\" \"<url>\"'; var_name='<url>'; var_value='https://www.baidu.com'; bash_script=bash_script.replace(var_name,var_value); print(bash_script)")
 echo $a
 header=$(curl -Is -X "GET" $url)
 echo "header:$header"
