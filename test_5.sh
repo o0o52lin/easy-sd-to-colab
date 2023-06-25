13540540142891054c3d6945302571d1792d0ae7
o0o52lin <345399240@qq.com>
2023-06-25 13:57:06 +0800

    Update and rename test.sh to test_5.sh

--- test_5.sh
+++ test_5.sh
@@ -17,4 +17,4 @@ if [[ $local_size != 0 ]] && [[ "$local_size" -eq "$remote_size" ]]; then
   echo "INFO: local file '$output_filename' is up-to-date, skipping download"
   return 0
 fi
-curl -Is -X GET $url
+curl -Is -X GET https://civitai-delivery-worker-prod-2023-06-01.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/749997/model/badPictures.Vlw2.pt?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22bad_pictures.pt%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2fea663d76bd24a496545da373d610fc/20230625/us-east-1/s3/aws4_request&X-Amz-Date=20230625T055636Z&X-Amz-SignedHeaders=host&X-Amz-Signature=f6ac4d50328cd8d58cc97aa2ebd9fa9fb7e9b358bfb245a52f349bcdc6abd589
