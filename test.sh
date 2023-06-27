9f00af21f1215c3c9b0e367b4c5f7cd902f4fe99
o0o52lin <345399240@qq.com>
2023-06-27 18:12:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,12 @@
 password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c20)
 
 # Download ngrok
-# wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O ngrok.tgz && sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/bin
+curl -s -X GET "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz" -o ngrok-v3-stable-linux-amd64.tgz
+tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 
 # Setup sshd
 apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-# apt-get -y install jq
+apt-get -y install jq
 
 # Set root password
 echo "root:$password" | chpasswd
@@ -22,10 +23,20 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 /usr/sbin/sshd -D &
 
 # Create tunnel
-ngrok authtoken '2RkIiHPgfucdreF63Z5L8P1BR3V_5RpsFfVRQNyDBSgTyUBxr' && ngrok tcp 22 &
-
+ngrok authtoken '2RkIiHPgfucdreF63Z5L8P1BR3V_5RpsFfVRQNyDBSgTyUBxr'
+nohup ngrok tcp 22 &
+sleep 2
 # Get public address and print connect command
-curl -s http://localhost:4040/api/tunnels
+res=$(curl -s http://localhost:4040/api/tunnels)
+str=$(echo $res | jq '.tunnels[0].public_url')
+
+# 使用sed命令和正则表达式替换字符串
+new_str=$(echo "$str" | sed 's/tcp:\/\///')
+
+# 使用cut命令提取子字符串
+host=$(echo "$new_str" | cut -d':' -f1)
+port=$(echo "$new_str" | cut -d':' -f2)
+
 echo "SSH command: ssh -p$port root@$host"
 
 # Print root password
