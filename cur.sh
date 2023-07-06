32103900fb5ba31edf8a4e39c20861894058722f
o0o52lin <345399240@qq.com>
2023-07-06 14:01:03 +0800

    Update cur.sh

--- cur.sh
+++ cur.sh
@@ -1,2 +1,2 @@
-
+#!/bin/bash
 curl -o /content/drive/MyDrive/SD/models/Stable-diffusion/v2-1_512-ema-pruned.safetensors https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.safetensors
