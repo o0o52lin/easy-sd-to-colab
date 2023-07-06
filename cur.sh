24f1ff21b2cf8b8cc726b3d4760c33d2ac08b4bd
o0o52lin <345399240@qq.com>
2023-07-06 14:00:41 +0800

    Create cur.sh

---
+++ cur.sh
@@ -0,0 +1,2 @@
+
+curl -o /content/drive/MyDrive/SD/models/Stable-diffusion/v2-1_512-ema-pruned.safetensors https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.safetensors
